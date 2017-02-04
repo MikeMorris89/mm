728eb79fade7ec04f93b5193211cc46354a73c41
mikemorris89 <mike.morris89@gmail.com>
2017-02-04 13:06:45 -0500

    new cmds

---
+++ cmd/set-session-type.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+f [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+  SESSION_TYPE=remote/ssh
+# many other tests omitted
+else
+  case $(ps -o comm= -p $PPID) in
+    sshd|*/sshd) SESSION_TYPE=remote/ssh;;
+  esac
+fi
